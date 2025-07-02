798ee3f031891d87fc6c813aceb4638a08295867
jiang <870041522@qq.com>
2025-07-02 15:32:46 +0900

    更改上传代码

--- php/upload.php
+++ php/upload.php
@@ -1,6 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 session_start();
 if (!isset($_SESSION['email']) || $_SESSION['is_admin'] != 1) {
     die("无权访问");
@@ -8,24 +6,22 @@ if (!isset($_SESSION['email']) || $_SESSION['is_admin'] != 1) {
 
 include('../inc/db.php');
 
-// 获取字段
 $title = $_POST['title'];
+$pic = $_FILES['pic'];
+$book = $_FILES['bookfile'];
 
-// 处理封面图
-$pic_name =$_FILES['pic']['name'];
-$pic_tmp = $_FILES['pic']['tmp_name'];
-move_uploaded_file($pic_tmp, "../ziyuan/images/" . $pic_name);
+if ($pic['error'] || $book['error']) {
+    die("上传失败");
+}
 
-// 处理图书文件
-$book_name =$_FILES['bookfile']['name'];
-$book_tmp = $_FILES['bookfile']['tmp_name'];
-move_uploaded_file($book_tmp, "../ziyuan/books/" . $book_name);
+// 移动上传文件
+move_uploaded_file($pic['tmp_name'], "../ziyuan/images/" . $pic['name']);
+move_uploaded_file($book['tmp_name'], "../ziyuan/books/" . $book['name']);
 
-// 写入数据库
-$stmt = $con->prepare("INSERT INTO ebooks (title, pic, file_path) VALUES (?, ?, ?)");
-$stmt->bind_param("sss", $title, $pic_name, $book_name);
-$stmt->execute();
+// 插入数据库
+$sql = "INSERT INTO ebooks (title, pic, file_path) 
+        VALUES ('$title', '{$pic['name']}', '{$book['name']}')";
+mysqli_query($con, $sql);
 
 echo "<script>alert('上传成功');location.href='../upload.html';</script>";
-
 ?>
